cd6650943cf1d00a25aabea628f91696c9f54e4c
Harsh <harshtanishq2002@gmail.com>
2024-06-15 13:50:07 +0530

    feat: link styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 const NavBar = () => {
+  // const navLinkStyle = ({isActive}) => {
+  //     return{
+  //         fontWeight : isActive ? 'bold' : 'normal',
+  //         textDecoration : isActive ? 'none' : 'underline',
+  //         color : isActive ? 'red' : 'blue',
+
+  //     }
+  // }
+
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
