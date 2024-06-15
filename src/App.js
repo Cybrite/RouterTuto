0ee9c953a15f993845f2a40943681ee891360106
Harsh <harshtanishq2002@gmail.com>
2024-06-15 13:36:50 +0530

    feat: added react router dom and made pathways

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
